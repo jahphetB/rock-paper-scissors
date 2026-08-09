d2d77a987ca30ff4495e3230e9590d2bcc9ae41a
jahphetB <japhetlakew098@gmail.com>
2026-08-08 19:34:06 -0600

    add buttons for rps for user input

--- script.js
+++ script.js
@@ -1,39 +1,71 @@
+// const { createElement } = require("react");
 
 console.log("hello world.");
 
 let humanScore = 0;
 let computerScore = 0;
 
-const hands = ["rock", "paper", "siccors"];
-function getComputerChoice(){
-    let ran_num = Math.floor(Math.random()*3);
+const hands = ["rock", "paper", "scissors"];
+
+function getComputerChoice() {
+    let ran_num = Math.floor(Math.random() * 3);
     return hands[ran_num];
 }
 
-function getHumanChoice(){
-    let user = prompt("Type one of these: rock, paper, or siccors");
-    return user.toLowerCase();
-}
-
-function playRound(human_hand, comp_hand){
-    if (human_hand === "rock" && comp_hand === "siccors"){
+function playRound(human_hand, comp_hand) {
+    if (human_hand === "rock" && comp_hand === "scissors") {
         humanScore += 1;
-        console.log("You win! Rock beats Siccors.");
+        results.textContent = "You win! Rock beats Scissors.";
     } else if (human_hand === "paper" && comp_hand === "rock") {
         humanScore += 1;
-        console.log("You win! Papper beats Rock.");
-    } else if (human_hand === "siccors" && comp_hand === "paper") {
+        results.textContent = "You win! Paper beats Rock.";
+    } else if (human_hand === "scissors" && comp_hand === "paper") {
         humanScore += 1;
-        console.log("You win! Siccors beats Paper.");
+        results.textContent = "You win! Scissors beats Paper.";
     } else if (human_hand === comp_hand) {
-        console.log(`It's a tie. ${human_hand} and ${comp_hand}. Try again.`)
+        results.textContent = `It's a tie. ${human_hand} and ${comp_hand}. Try again.`;
     } else {
         computerScore += 1;
-        console.log(`You lost! ${comp_hand} beats ${human_hand}.`);
+        results.textContent = `You lost! ${comp_hand} beats ${human_hand}.`;
+    }
+
+    scores.textContent = `${humanScore}, ${computerScore}`;
+}
+
+const content = document.querySelector("body");
+
+const results = document.createElement("div");
+content.append(results);
+
+const scores = document.createElement("div");
+scores.textContent = `${humanScore}, ${computerScore}`;
+content.append(scores);
+
+function checker() {
+    if (humanScore >= 5 || computerScore >= 5) {
+        return false;
+    } else {
+        return true;
     }
 }
 
-for (let index = 0; index < 5; index++) {
-    let play = playRound(getHumanChoice(), getComputerChoice());  
-}
-console.log(`human: ${humanScore}, computer: ${computerScore}`);
+const buttons = document.querySelectorAll("button");
+
+buttons.forEach(button => {
+    button.addEventListener("click", (event) => {
+        event.preventDefault();
+
+        let comp = getComputerChoice();
+        let human = event.target.textContent;
+
+        if (checker()) {
+            playRound(human, comp);
+
+            if (humanScore >= 5) {
+                results.textContent = "You won the game!";
+            } else if (computerScore >= 5) {
+                results.textContent = "Computer won the game!";
+            }
+        }
+    });
+});
